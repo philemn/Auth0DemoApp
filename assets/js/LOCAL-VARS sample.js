0ba1796dc3fcbc9ae471c3a57b4fa905ef880f1f
marcopolox <nesopo@gmail.com>
2022-07-13 18:38:38 -0500

    dev

--- assets/js/LOCAL-VARS sample.js
+++ assets/js/LOCAL-VARS sample.js
@@ -15,3 +15,4 @@ const ORG_B_ID = "";
 const enterprise_authorization_url = "https://login.microsoftonline.com/xxxxxxxxxx";
 const enterprise_authorization_name = "Azure AD";
 const android_appetize_url = "https://appetize.io/embed/xxxxxxxxx"
+const GOOGLE_CONNECTION_NAME="google-oauth2"
